f9d45caed2b942dcabc4d385c127a296ba7ff309
Sean Newman <seangnewman@msn.com>
2018-09-23 22:27:26 -0700

    Add project files.

---
+++ PluralSight_Intro_to_Collections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluralSight_Intro_to_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
